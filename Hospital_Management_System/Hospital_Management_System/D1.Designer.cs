5876f1bb8fb1f22311b8c07556cda2b54e344f32
NikhileshSinha <78151367+NikhileshSinha@users.noreply.github.com>
2021-04-02 19:30:00 +0530

    Add files via upload

---
+++ Hospital_Management_System/Hospital_Management_System/D1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Hospital_Management_System
+{
+    partial class D1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.t1 = new System.Windows.Forms.TextBox();
+            this.t2 = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(351, 555);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Take Input";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Click_Input);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Your Username :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F);
+            this.label2.Location = new System.Drawing.Point(198, 390);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter your password :";
+            // 
+            // t1
+            // 
+            this.t1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F);
+            this.t1.Location = new System.Drawing.Point(513, 266);
+            this.t1.Name = "t1";
+            this.t1.Size = new System.Drawing.Size(188, 34);
+            this.t1.TabIndex = 5;
+            // 
+            // t2
+            // 
+            this.t2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.t2.Location = new System.Drawing.Point(513, 381);
+            this.t2.Name = "t2";
+            this.t2.PasswordChar = '@';
+            this.t2.Size = new System.Drawing.Size(188, 34);
+            this.t2.TabIndex = 6;
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(785, 12);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 23);
+            this.b1.TabIndex = 7;
+            this.b1.Text = "Back";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.Click_Back);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hospital_Management_System.Properties.Resources.ssL_ICN;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 152);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(178, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(560, 54);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "DOCTOR\'S LOGIN PAGE";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(510, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(355, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "(HINT : username : Doctor               and               Password: sslhospital  " +
+    "  )";
+            // 
+            // D1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(872, 833);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.t2);
+            this.Controls.Add(this.t1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "D1";
+            this.Text = "D1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox t1;
+        private System.Windows.Forms.TextBox t2;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Hospital_Management_System/Hospital_Management_System/D2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Hospital_Management_System
+{
+    public partial class D2 : Form
+    {
+        SqlConnection con = new SqlConnection(Properties.Settings.Default.HMSConnectionString);
+        public D2()
+        {
+            InitializeComponent();
+            display();
+            DateTime now = DateTime.Now;
+            l4.Text = now.ToString("F"); 
+        }
+
+        private void Click_back(object sender, EventArgs e)
+        {
+
+            D1 ob = new D1();
+            ob.Show();
+            this.Hide();
+        }
+
+        public void display()
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = "select * from Patitent_info ";
+            cmd.ExecuteNonQuery();
+            DataTable dt1 = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt1);
+            dg1.DataSource = dt1;
+            con.Close();
+        }
+
+        private void Click_del(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandText = "delete from Patitent_info where Patient_name = @nm";
+            cmd.Parameters.AddWithValue("@nm", t1.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Entities has been sucessfully deleted !!!! ");
+            con.Close();
+            display();
+        }
+
+        private void Click_ref(object sender, EventArgs e)
+        {
+            D2 obb = new D2();
+            obb.Show();
+            this.Hide();
+        }
+    }
+}
